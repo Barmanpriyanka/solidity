3a58f703a484b473cc6704118c56e0b5534b8e61
Priyanka Barman <130579302+Barmanpriyanka@users.noreply.github.com>
2024-07-06 12:26:49 +0530

    Create payable.sol
    One of the types of function in solidity

---
+++ payable.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract payableContract {
+
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function deposit () external payable {}
+
+    function getBalance () external view returns (uint) {
+        return address(this).balance;
+    }
+    
+
+}
